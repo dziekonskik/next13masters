b8182471326f7b643eb5faceabc2cbef38eaf597
Konrad Dziekonski <jaseveen@gmail.com>
2023-09-14 19:15:42 +0200

    fix path

--- src/components/molecules/ProductListItem.tsx
+++ src/components/molecules/ProductListItem.tsx
@@ -10,7 +10,7 @@ type ProductListItemProps = {
 export const ProductListItem = ({ product }: ProductListItemProps) => {
 	return (
 		<li className="flex flex-col items-center justify-between p-4 bg-white rounded-lg shadow-lg">
-			<Link href={`/products/${product.id}`}>
+			<Link href={`/product/${product.id}`}>
 				<ProductImage src={product.image} alt={product.title} />
 				<ProductListItemDescription details={product} />
 			</Link>
