5cb63ef221d4604ab27368b0ebbcd83afcdd6904
Konrad Dziekonski <jaseveen@gmail.com>
2023-09-19 18:32:12 +0200

    redirect in next.config, fix active link types

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        typedRoutes: true,
-    },
+	experimental: {
+		typedRoutes: true,
+	},
+	redirects: async () => [{ source: "/products", destination: "/products/1", permanent: true }],
 };
 
 module.exports = nextConfig;
--- src/components/atoms/ActiveLink.tsx
+++ src/components/atoms/ActiveLink.tsx
@@ -1,23 +1,29 @@
 "use client";
+import { type UrlObject } from "url";
 import Link from "next/link";
-import { type FC } from "react";
 import { usePathname } from "next/navigation";
 import type { Route } from "next";
 import clsx from "clsx";
-
-interface ActiveLinkProps {
+interface ActiveLinkProps<T extends string = string> {
 	children: React.ReactNode;
-	href: Route;
+	href: Route<T> | UrlObject;
+	exact?: boolean;
 }
 
-export const ActiveLink: FC<ActiveLinkProps> = ({ children, href }) => {
+export const ActiveLink = <T extends string>({
+	children,
+	href,
+	exact = false,
+}: ActiveLinkProps<T>) => {
 	const pathname = usePathname();
+	const stringPathname = typeof href === "object" ? href.pathname ?? "" : href;
+	const isActive = exact ? pathname === stringPathname : pathname.includes(stringPathname);
 
 	return (
 		<Link
 			href={href}
 			className={clsx("text-blue-500 hover:text-blue-600 border-blue-600", {
-				"border-b": pathname === href,
+				"border-b": isActive,
 			})}
 		>
 			{children}
