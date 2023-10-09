6442dab730bfae50fd3557f375e6dc71b14ea342
Konrad Dziekonski <jaseveen@gmail.com>
2023-10-09 08:12:15 +0200

    add slugify helper fn

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,12 @@
 export function isParsableToNumber(str: string) {
 	return !isNaN(parseFloat(str));
 }
+
+export function slugify(input: string): string {
+	return input
+		.trim() // Trim whitespace from the beginning and end
+		.toLowerCase() // Convert to lowercase
+		.replace(/[^a-z0-9\- ]/g, "") // Remove any character that is not a-z, 0-9, hyphen, or space
+		.replace(/\s+/g, "-") // Replace spaces with hyphens
+		.replace(/-+/g, "-"); // Replace multiple hyphens with a single one
+}
